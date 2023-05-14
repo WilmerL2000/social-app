93983077135dd45f5a2c81a2714f78bb9c7091bf
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2023-05-14 15:32:51 -0600

    Fixing map problem and modifying Readme file

--- client/src/scenes/widgets/SearchWidget.jsx
+++ client/src/scenes/widgets/SearchWidget.jsx
@@ -92,16 +92,15 @@ const SearchWidget = () => {
           </>
         ) : (
           users?.map((friend) => (
-            <>
+            <Box key={friend._id}>
               <Divider />
               <Friend
-                key={friend._id}
                 friendId={friend._id}
                 name={`${friend.firstName} ${friend.lastName}`}
                 subtitle={friend.occupation}
                 userPicturePath={friend.picturePath}
               />
-            </>
+            </Box>
           ))
         )}
       </Box>
